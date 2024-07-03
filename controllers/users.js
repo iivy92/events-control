69d8abcc9bc698d3f793fd269eaddf3243490d4b
Pedro Santana <pedro.santana@ilia.digital>
2024-07-03 16:38:09 -0300

    adding delete endpoint

--- controllers/users.js
+++ controllers/users.js
@@ -29,8 +29,8 @@ const usersController = {
                 return
             }
 
-
             const response = await usersModel.create(user)
+
             res.status(201).json({response, msg: "User created successfully"})
 
         } catch (error) {
@@ -47,6 +47,39 @@ const usersController = {
             console.log(error)
         }
     },
+
+    get: async(req, res) => {
+        try {
+            const cpf = req.params.cpf
+
+            const response = await usersModel.findOne({cpf: cpf})
+            res.json(response)
+            
+        } catch (error) {
+            console.log(error)
+        }
+    },
+
+    delete: async(req, res) => {
+        try {
+            const cpf = req.params.cpf
+
+            const user = await usersModel.findOne({cpf: cpf})
+
+            if ( !user ){
+                res.status(404).json({msg: "User not found"})
+                return
+            }
+
+            deletedUser = await usersModel.findByIdAndDelete(user.id)
+
+            res
+                .status(200)
+                .json({deletedUser, msg: "User deleted successfully"})
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
 }
 
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,8 @@ const usersController = require("../controllers/users")
 
 router.route("/users").post((req, res) => usersController.create(req, res))
 router.route("/users").get((req, res) => usersController.getAll(req, res))
+router.route("/users/:cpf").get((req, res) => usersController.get(req, res))
+router.route("/users/:cpf").delete((req, res) => usersController.delete(req, res))
+
 
 module.exports = router
